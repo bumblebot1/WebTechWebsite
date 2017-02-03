cab803ac9a2080c0f51eda17e81d1ac860b2f218
bumblebot <rm14834@my.bristol.ac.uk>
2017-02-03 02:00:00 +0000

    fixed some bad tabbing issues

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ var config = require('./config.js')
 
 //determine name of platform main browser
 var browser = os.platform() === 'linux' ? 'google-chrome' : (
-							os.platform() === 'darwin' ? 'google chrome' : (
-							os.platform() === 'win32' ? 'chrome' : 'firefox'));
+              os.platform() === 'darwin' ? 'google chrome' : (
+              os.platform() === 'win32' ? 'chrome' : 'firefox'));
 
 gulp.task('start', startServer);
 
@@ -19,33 +19,33 @@ gulp.task('test:server', testServer);
 gulp.task('test:client', testClient);
 
 function startServer(){
-	var exec = 'node';
+  var exec = 'node';
 
-	nodemon({
-		script: config.server.src,
-		exec: exec
-	});
+  nodemon({
+      script: config.server.src,
+      exec: exec
+  });
 
-	gulp.src('')
-			.pipe(open({app: browser, uri: config.server.address}));
+  gulp.src('')
+      .pipe(open({app: browser, uri: config.server.address}));
 }
 
 function testServer(){
-	var testFiles = config.serverTests.files;
-	gulp.src(testFiles)
-		  .pipe(watch(testFiles))
-		  .pipe(jasmineBrowser.specRunner())
-			.pipe(jasmineBrowser.server({port: config.serverTests.port}));
-	gulp.src('')
-			.pipe(open({app: browser, uri: config.serverTests.address}));
+  var testFiles = config.serverTests.files;
+  gulp.src(testFiles)
+      .pipe(watch(testFiles))
+      .pipe(jasmineBrowser.specRunner())
+      .pipe(jasmineBrowser.server({port: config.serverTests.port}));
+  gulp.src('')
+      .pipe(open({app: browser, uri: config.serverTests.address}));
 }
 
 function testClient(){
-	var testFiles = config.clientTests.files
-	gulp.src(testFiles)
-		  .pipe(watch(testFiles))
-		  .pipe(jasmineBrowser.specRunner())
-			.pipe(jasmineBrowser.server({port: config.clientTests.port}));
-	gulp.src('')
-			.pipe(open({app: browser, uri: config.clientTests.address}));
+  var testFiles = config.clientTests.files
+  gulp.src(testFiles)
+      .pipe(watch(testFiles))
+      .pipe(jasmineBrowser.specRunner())
+      .pipe(jasmineBrowser.server({port: config.clientTests.port}));
+  gulp.src('')
+      .pipe(open({app: browser, uri: config.clientTests.address}));
 }
